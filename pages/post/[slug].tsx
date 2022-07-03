4d7afb9a713a00ca793fc46e7a0f904878a655cd
iamatnuria <ignasiamat10@gmail.com>
2022-07-03 16:26:14 +0200

    Form comments added, validation added, comment box section added. Ready to deploy to production

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,15 +1,47 @@
 import { GetStaticProps } from 'next'
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
 interface Props {
     post:  Post; 
 }
 
 export default function Post({ post }: Props) {
+
+  const [submitted, setSubmitted] = useState(false);
+
+    console.log(post);
+    
+
+  const { 
+    register,
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<IFormInput>()
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then(() => {
+      console.log(data);
+      setSubmitted(true);
+    }).catch((err) => {
+      console.log(err);
+      setSubmitted(false);
+    })
+  };
+
   return (
     <main>
         <Header />
@@ -57,6 +89,69 @@ export default function Post({ post }: Props) {
             </div>
 
           </article>
+
+          <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
+
+          {submitted ? (
+              <div className='flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto'>
+                <h3 className='text-3xl font-bold'>
+                  Thank you for submitting your comment!
+                </h3>
+                <p>Once it has been approved, it will appear below!</p>
+              </div>
+          ): (
+<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col p-5 max-w-2xl mx-auto mb-10' action="">
+            <h3 className='text-sm text-yellow-500'>Enjoyed this article?</h3>
+            <h4 className='text-3xl font-extrabold text-[#292929]'>Leave a comment below!</h4>
+            <hr className='py-3 mt-2' />
+
+            <input
+                {...register("_id")}
+                type="hidden"
+                name="_id"
+                value={post._id}
+              />
+
+            <label className='block mb-5'>
+              <span className='text-gray-700'>Name</span>
+              <input {...register("name", {required: true})} 
+              className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring-1 focus:outline-none' type="text" name="name" placeholder='John Appleseed' />
+            </label>
+            <label className='block mb-5'>
+              <span className='text-gray-700'>Email</span>
+              <input {...register("email", {required: true})} 
+              className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring-1 focus:outline-none' placeholder='John Appleseed' type="text" />
+            </label>
+            <label className='block mb-5'>
+              <span className='text-gray-700'>Comment</span>
+              <textarea {...register("comment", {required: true})}
+              className='shadow border rounded py-2 px-3 mt-1 block w-full ring-yellow-500 focus:ring-1 focus:outline-none' placeholder='John Appleseed' rows={8} />
+            </label>
+
+            {/* errors will return when field validation fails */}
+
+            <div className='flex flex-col p-5'>
+              {errors.name && (<span className='text-red-500'>- The Name field is required</span>)}
+              {errors.comment && (<span className='text-red-500'>- The Comment field is required</span>)}
+              {errors.email && (<span className='text-red-500'>- The Email field is required</span>)}
+            </div>
+
+            <input className='shadow bg-yellow-500 hover:bg-yellow-400 transition-colors focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer' type="submit"  />
+          </form>
+          )}
+
+          {/* Comments */}
+          <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+            <h3 className='text-4xl font-extrabold text-[#292929]'>Comments</h3>
+            <hr className='pb-2' />
+
+            {post.comments.map((comment) => (
+              <div key={comment._id}>
+                <p><span className='text-yellow-500'>{comment.name}: </span>{comment.comment}</p>
+              </div>
+            ))}
+          </div>
+          
     </main>
   );
 }
@@ -85,18 +180,22 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
     const query = `*[_type == "post" && slug.current == $slug][0]{
-        _id,
-        _createdAt,
-        title,
-        author-> {
-          name,
-          image
-        },
-        description,
-        mainImage,
-        slug,
-        body
-      }`
+      _id,
+      _createdAt,
+      title,
+      author-> {
+        name,
+        image
+      },
+        'comments': *[
+          _type == "comment" &&
+          post._ref == ^._id &&
+          approved == true],
+      description,
+      mainImage,
+      slug,
+      body
+    }`
 
       const post = await sanityClient.fetch(query, {
         slug: params?.slug,
--- typings.d.ts
+++ typings.d.ts
@@ -5,7 +5,8 @@ export interface Post {
     author: {
         name: string;
         image: string;
-    },
+    };
+    comments: Comment[];
     description: string;
     mainImage: {
         asset: {
@@ -16,4 +17,20 @@ export interface Post {
         current: string;
     };
     body: [object];
+}
+
+export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string;
+    name: string;
+    post: {
+        _ref: string;
+        _type: string;
+    };
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
 }
